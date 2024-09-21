4f8ca484b29f78f7206113d2c2b4e0b42189a8e4
akhsuna31 <akhsuna31@gmail.com>
2024-09-21 13:26:32 +0530

    square star

---
+++ DAY_4/PatternPrinting.java
@@ -0,0 +1,15 @@
+package DAY_4;
+import java.util.*;
+public class PatternPrinting {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        for(int i=1; i<=n;i++){
+            for(int j=1; j<=n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
