f41bf448ecf898fb96470d6c7a1d3dfc88875ffd
akhsuna31 <akhsuna31@gmail.com>
2024-09-28 15:01:44 +0530

    array

--- DAY_6/Arrays.java
+++ DAY_6/Arrays.java
@@ -3,10 +3,43 @@ import java.util.*;
 
 public class Arrays {
     static void swap(int[] array, int i, int j){
-        int temp= array[i];
-        array[i]= array[j];
-        array[j]=temp;
+        // int temp= array[i];
+        // array[i]= array[j];
+        // array[j]=temp;
+        array[i]=array[i]+array[j];
+        array[j]=array[i]-array[j];
+        array[i]=array[i]-array[j];
     }
+    static void reverse1(int[] array){
+        int len= array.length;
+        int[] reverseArray= new int[len];
+        for(int i=0; i<len; i++){
+            reverseArray[i]=array[len-1-i];
+        }
+
+        //shallow copy
+        //array=reverseArray;
+        
+        //deep copy
+        for(int i=0; i<len; i++){
+            array[i]=reverseArray[i];
+        }
+
+        for (int a : array) {
+            System.out.print(a+" ");
+        }
+    }
+
+    static void reverse2(int[] array){
+        int n = array.length;
+        for(int i=0; i<n/2; i++){
+            swap(array, i, n-1-i);
+        }
+        for(int i: array){
+            System.out.print(i+" ");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int len= sc.nextInt();
@@ -15,19 +48,23 @@ public class Arrays {
             arr[i]=sc.nextInt();
         }
         
-        System.out.println("Your Array before swapping is:");
+        System.out.println("Your Array is:");
         for(int i: arr){
             System.out.print(i+ " ");
         }
         System.out.println();
-        System.out.println("Enter value of the indexes you want to swap:");
-        int i=sc.nextInt();
-        int j = sc.nextInt();
-        swap(arr, i,j);
-        System.out.println("Your Array after swapping is:");
-        for(int a: arr){
-            System.out.print(a+ " ");
-        }
+        // System.out.println("Enter value of the indexes you want to swap:");
+        // int i=sc.nextInt();
+        // int j = sc.nextInt();
+        // swap(arr, i,j);
+        // System.out.println("Your Array after swapping is:");
+        // for(int a: arr){
+        //     System.out.print(a+ " ");
+        // }
+
+        System.out.println("Your reverse array is: ");
+        reverse2(arr);
+
         sc.close();
     }
 }
